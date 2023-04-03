13bfc5bcde40579aa091dc2128496ed0ecf998f4
schallerv <schaller.v@northeastern.edu>
2023-04-03 16:10:51 -0400

    full fix slider

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // constants for plot design
 const FRAME_HEIGHT = 510;
-const FRAME_WIDTH = 850; 
-const MARGINS = {left: 50, right: 10, top: 25, bottom: 25};
+const FRAME_WIDTH = 900; 
+const MARGINS = {left: 50, right: 50, top: 25, bottom: 25};
 
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
 const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right; 
 
 const SLIDE_HEIGHT = 115;
-const SLIDE_WIDTH = 850; 
-const SLIDE_MARGINS = {left: 50, right: 10, top: 10, bottom: 20};
+const SLIDE_WIDTH = 900; 
+const SLIDE_MARGINS = {left: 50, right: 50, top: 10, bottom: 20};
 
 const SLIDE_VIS_H = SLIDE_HEIGHT - MARGINS.top - MARGINS.bottom;
 const SLIDE_VIS_W = SLIDE_WIDTH - MARGINS.left - MARGINS.right; 
@@ -322,8 +322,11 @@ function build_plots() {
         console.log(slideMax)
 
         // Create new data with the selection?
-        const dataFilter = data.filter(function(row){
+        let dataFilter1 = data.filter(function(row){
             return row['DATE'] >= slideMin});
+
+        let dataFilter = dataFilter1.filter(function(row){
+            return row['DATE'] <= slideMax});
 
         const domain = [slideMin, slideMax];
 
